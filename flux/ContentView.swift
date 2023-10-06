79c49cdf5ca8ffa62fb6cf50251b4fc89e8c3774
siavava <amittaijoel@outlook.com>
2023-10-06 16:53:03 -0400

    init

---
+++ flux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flux
+//
+//  Created by Amittai on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Okay this is neat!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flux/fluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluxApp.swift
+//  flux
+//
+//  Created by Amittai on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct fluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
